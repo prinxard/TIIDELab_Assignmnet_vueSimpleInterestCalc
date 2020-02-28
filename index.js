da78e01597696d0e6333dd7b9e2a92d55ddfd70d
prinxard <prinxard@gmail.com>
2020-02-28 12:47:55 +0100

    First comit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var calculator = new Vue({
+el: '#calculator',
+data: {
+principal: '',
+rate: '',
+time: '',
+calcPayment: '',
+computeReset: ''
+},
+computed: {
+calcInterest: function(e){
+    var p = this.principal;
+    var r = this.rate;
+    var t = this.time;
+    var SI = ( p * r * t) / 100;
+    return (SI).toFixed(2);
+}
+},
+methods: {
+computeReset: function(e){
+this.principal = 0;
+this.rate = 0;
+this.time = 0;
+done = false
+}
+}
+});
